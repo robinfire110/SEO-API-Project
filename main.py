d37169a4c3a3479c06fcbb83bb3c4b823a0d5917
Andres Villasmil <andyevillasmil@gmail.com>
2022-06-28 21:45:22 +0000

    Setting up restart

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-#from pyyoutube import Api
-#api = Api(api_key='AIzaSyCjRbmEAeEe35cuqAsoJ7Zn80ukUx0FQjQ')
+from pyyoutube import Api
+api = Api(api_key='AIzaSyCjRbmEAeEe35cuqAsoJ7Zn80ukUx0FQjQ')
 
-#channel_by_id = api.get_channel_info(channel_id="UC_x5XG1OV2P6uZZ5FSM9Ttw")
-#print(channel_by_id.items)
-
-from pytwitter import Api
-
+channel_by_id = api.get_channel_info(channel_id="UC_x5XG1OV2P6uZZ5FSM9Ttw")
+print(channel_by_id.items)
