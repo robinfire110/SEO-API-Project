53c7e65bb7e343a60732cb546f7c65c233afb216
Andres Villasmil <andyevillasmil@gmail.com>
2022-06-28 22:33:14 +0000

    YouTube Search v1

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from pyyoutube import Api
-api = Api(api_key='AIzaSyCjRbmEAeEe35cuqAsoJ7Zn80ukUx0FQjQ')
+import requests
+import json
 
-channel_by_id = api.get_channel_info(channel_id="UC_x5XG1OV2P6uZZ5FSM9Ttw")
-print(channel_by_id.items)
+#Get Input
+channel_name = input("Please input name of YouTube Channel to search: ")
+r = requests.get(f'https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername={channel_name}&key=AIzaSyCjRbmEAeEe35cuqAsoJ7Zn80ukUx0FQjQ')
+data = r.json()
+
+#Print
+if data['pageInfo']['totalResults'] != 0:
+  item = data['items'][0]['statistics']
+  print(f"{channel_name} has {item['subscriberCount']} subscribers, {item['videoCount']} total videos and a grand sum of {item['viewCount']} across their whole channel!")
+else:
+  print("Either that YouTube Channel does not exists or they are not big enough to be searched by name. Please try again.")
